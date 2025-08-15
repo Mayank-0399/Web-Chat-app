4ffbb90d0b5812943bb2da1d3a518938866e465b
Mayank-0399 <mayanksingh230651@gmail.com>
2025-08-15 16:29:34 +0530

    Update index.js

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -14,22 +14,21 @@ const io = new Server(server, {
 
 const users = {};
 
-// Serve all frontend files (index.html, css, js, images)
-app.use(express.static('.'));
+app.use(express.static(__dirname));
 
-// Explicitly serve index.html for root route
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Handle Socket.IO connections
+
 io.on('connection', socket => {
     socket.on('new-user-joined', name => {
         console.log(`${name} joined the chat`);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
 
-        // Send list of currently online users (excluding the new one)
+        
         const otherUsers = Object.values(users).filter(n => n !== name);
         socket.emit('currently-online', otherUsers);
     });
@@ -47,7 +46,7 @@ io.on('connection', socket => {
     });
 });
 
-// Use Render's provided port or default to 8000 locally
+
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
