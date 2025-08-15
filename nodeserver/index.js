8254e8c68429e8e5e3868315e28965490dafa642
Mayank-0399 <mayanksingh230651@gmail.com>
2025-08-15 16:49:39 +0530

    Update index.js

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,34 +1,30 @@
 const express = require('express');
-const app = express();
 const http = require('http');
-const server = http.createServer(app);
-const { Server } = require('socket.io');
+const socketIo = require('socket.io');
+const path = require('path');
 
-// Create Socket.IO server with CORS enabled
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
+const app = express();
+const server = http.createServer(app);
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname, '../')));
+
+const PORT = process.env.PORT || 8000;
+
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
 });
 
 const users = {};
-
-app.use(express.static(__dirname));
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 
 io.on('connection', socket => {
     socket.on('new-user-joined', name => {
         console.log(`${name} joined the chat`);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
-
-        
         const otherUsers = Object.values(users).filter(n => n !== name);
         socket.emit('currently-online', otherUsers);
     });
@@ -36,18 +32,16 @@ io.on('connection', socket => {
     socket.on('send', message => {
         socket.broadcast.emit('receive', {
             message: message,
-            name: users[socket.id]
+            name: users[socket.id],
         });
     });
 
-    socket.on('disconnect', () => {
+    socket.on('disconnect', message => {
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
 });
 
-
-const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
